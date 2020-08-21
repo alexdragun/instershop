b14991b0b9747a340979f4a08dac3125c0bb7c36
Alex Dragun <alex.dragun@gauss.hr>
2020-08-21 17:32:15 +0200

    46b3cf9

---
+++ _nuxt/pages/shop/index.7b19df2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{211:function(t,n,e){"use strict";e.r(n);var r={},l=e(2),component=Object(l.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticStyle:{"margin-top":"100px"}},[this._v("\n  "+this._s(this.$route.params)+"\n")])}),[],!1,null,null,null);n.default=component.exports}}]);
